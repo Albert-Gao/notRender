7dfbce0fa8208af7daa63a3ccfd296a2e2bdd4d3
Albert-Gao <albertgaohy@gmail.com>
2018-11-06 22:30:42 +1300

    Start to add data for CityDropDown

--- src/store/createStepsToRender.ts
+++ src/store/createStepsToRender.ts
@@ -24,23 +24,6 @@ export default (callback: Function): IStep[] => [
     },
   },
   {
-    component: Chat,
-    position: LEFT,
-    props: {
-      text: '你好，我是COCO，请问你来自中国哪个地方？',
-      callback,
-    },
-  },
-  {
-    component: Chat,
-    position: LEFT,
-    props: {
-      text:
-        '我知道了，你希望来新西兰学习什么专业领域呢？',
-      callback,
-    },
-  },
-  {
     component: CityDropDown,
     position: RIGHT,
     props: {
