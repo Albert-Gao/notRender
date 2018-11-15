5cf5fac2d89c2e16b9448ce75a56ccf0711339eb
Albert-Gao <albertgaohy@gmail.com>
2018-11-15 21:10:03 +1300

    Update

--- src/screens/ChatScreen/components/LocationDropDown/LocationDropDown.tsx
+++ src/screens/ChatScreen/components/LocationDropDown/LocationDropDown.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { observer } from 'mobx-react-lite';
 import { IBaseStepType } from '../../utils';
 import ProvincesDropDown from './components/ProvincesDropDown';
 import CitiesDropDown from './components/CitiesDropDown';
@@ -7,6 +8,7 @@ import { FlexCard } from '../../../../components/buildingBlocks';
 const LocationDropDown: React.FC<IBaseStepType> = ({
   callback,
 }) => {
+  console.log('i am LocationDropDown');
   return (
     <FlexCard boxShadow={2}>
       <ProvincesDropDown />
@@ -15,4 +17,4 @@ const LocationDropDown: React.FC<IBaseStepType> = ({
   );
 };
 
-export default LocationDropDown;
+export default observer(LocationDropDown);
